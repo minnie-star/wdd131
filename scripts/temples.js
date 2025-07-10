6c95d4ad4959886de0fda01040cfe46a4a53f620
minnie-star <minenhleminnie8@gmail.com>
2025-07-10 20:42:24 +0200

    week02

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,16 @@
-const mainnav = document.querySelector('.navigation')
+/*const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#hamburger-menu');
 
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
-});
+});*/
+
+
+  const hamburger = document.querySelector('.hamburger-menu');
+  const header = document.querySelector('header');
+
+  hamburger.addEventListener('click', () => {
+    header.classList.toggle('hamburger-active');
+
+  });
